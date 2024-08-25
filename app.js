33d38b8406f3a1962aaf43259d67399614207e40
Talha Khalid <F2021266625@umt.edu.pk>
2024-08-25 16:58:49 +0500

    Port Configuration

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const { isAuthenticated, isAdminAuthenticated } = require('./middlewares/session
 const faviconMiddleware = require('./middlewares/favicon');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 
